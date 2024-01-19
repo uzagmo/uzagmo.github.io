55e881ab2180f39a29b8cbf5b0f7d04fa7e3ba8d
gyembo dorji <ugyenzangmo@Ugyens-MacBook-Air.local>
2024-01-19 12:09:25 -0400

    modified createProject function

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 //FUNCTION TO CREATE A PROJECT ELEMENT
-let createProject = (parent, title, picture, desc) => {
-    console.log(parent);
-    let proj = document.createElement("div");
-    parent.appendChild(proj);
+let createProject = (title, picture, desc) => {
 
+    let proj = document.createElement("div");
+    
     let projTitle = document.createElement("h2");
     proj.appendChild(projTitle);
-
     projTitle.textContent = title;
+
+    let projImg = document.createElement("div");
+    displayImages(projImg, picture);
+    proj.appendChild(projImg)
+
+    let projDesc = document.createElement("p");
+    projDesc.textContent = desc;
+    proj.appendChild(projDesc);
+
+    return proj;
+}
+
+//FUNCTION TO DISPLAY THE IMAGES IN THE PROJECT ELEMENT
+let displayImages = (parent, imgList) => {
 
 
 }
 
+
+
+//get the body
 let body = document.querySelector("body");
 
 //MAIN HEADER
@@ -22,14 +37,26 @@ mainHeader.textContent = "Group Projects";
 body.appendChild(mainHeader);
 
 
-//LIST OF PROJECTS
+//LIST OF PROJECTS: structure : <li> <div> <h2> </> <img> </> <p> </> </div> </li> 
 let list = document.createElement("ul")
 list.classList.add("projList");
 body.appendChild(list);
 
-let listitem = document.createElement("li");
-list.appendChild(listitem);
-createProject(listitem, "CS2063 - Intro to Android Development", 1, 1);
+//DATA
+let title = ["CS2063 - Intro to Android Development", "SWE4103 - Software Quality and Project Management", "TME 3313 - Managing Engineering and IT Projects","CS2043 - Software Engineering I"];
+
+
+//CREATING AND APPENDING PROJECT ELEMENTS
+for(i = 0; i < title.length; i++ ){
+    let listitem = document.createElement("li");
+    list.appendChild(listitem);
+
+    let listDiv = document.createElement("div");
+    listDiv = createProject(title[i], 1, 1);
+    listitem.appendChild(listDiv);
+
+}
+
 
 
 
